dac27958362509859c83d6496cae1d03947d0a7d
James Huang <james1530182gmail.com>
2024-05-28 10:59:57 -0500

    fixed changes

--- thread_utils.h
+++ thread_utils.h
@@ -6,24 +6,24 @@
 #include <atomic>
 #include <thread>
 #include <unistd.h>
-#include <sys/syscall.h>
+//#include <sys/syscall.h>
 
 namespace Common{
-    incline auto setThreadCore(int core_id) noexcept {
+    inline auto setThreadCore(int core_id) noexcept {
         cpu_set_t cpuset;
-        CPU_ZERO(&cupset);
+        CPU_ZERO(&cpuset);
         CPU_SET(core_id, &cpuset);
         return (pthread_setaffinity_np(pthread_self(), sizeof
     (cpu_set_t), &cpuset) == 0);
     }
 }
 
-template <typename T, typename ... A>
-incline auto createAndStartThread(int core_id, const std::string &name, T
-&&func, A &&... args) noexcept {
+template <typename T, typename A>
+inline auto createAndStartThread(int core_id, const std::string &name, T
+&&func, int B, int C, bool args) noexcept {
     std::atomic<bool> running(false), failed(false);
     auto thread_body = [&] {
-        if (core_id >= 0 && !setThreadCore (core_id)) {
+        if (core_id >= 0 && !setThreadCore(core_id)) {
             std::cerr << "Failed to set core affinity for " <<
             name << " " << pthread_self() << " to " << core_id
             << std::endl;
@@ -33,18 +33,18 @@ incline auto createAndStartThread(int core_id, const std::string &name, T
         std::cout << "Set core affinity for " << name << " " <<
         pthread_self() << " to " << core_id << std::endl;
         running = true;
-        std::forward<T>(func) ((std::forward)<A>(args))...);
+        std::forward<T>(func) ((std::forward<A>(args)));
     };
 
     auto t = new std::thread(thread_body);
     while (!running && !failed) {
         using namespace std::literals::chrono_literals;
-        std::this_thread::sleep_for(ls);
+        std::this_thread::sleep_for(1s);
     }
     if (failed) {
         t->join();
         delete t;
-        t = nulllptr;
+        t = nullptr;
     }
     return t;
 }
