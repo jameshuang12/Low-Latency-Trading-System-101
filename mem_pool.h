537e6f199b861cb22a495b1f2a5baee42d83a0c2
James Huang <james153018@gmail.com>
2024-06-02 10:21:10 -0500

    Add public big 3 and private function
    Initialized the memory pool and deleted default, copy, and move
    constructor methods. Also implemented updatenewfreeindex function
    to point to the next available free block

--- mem_pool.h
+++ mem_pool.h
@@ -14,5 +14,42 @@ namespace Common {
         };
         std::vector<ObjectBlock> store_;
         size_t next_free_index_ = 0;
+
+        auto updateNextFreeIndex() noexcept {
+            const auto initial_free_index = next_free_index_;
+            while (!store_[next_free_index_].is_free_) {
+                ++next_free_index_;
+                if (UNLIKELY(next_free_index_ == store_.size())) {
+                    // hardware branch predictor should almost always predict this to be false
+                    next_free_index_ = 0;
+                }
+                if (UNLIKELY(initial_free_index == next_free_index_)) {
+                    ASSERT(initial_free_index != next_free_index_, "Memory Pool out of space");
+                }
+            }
+        }
+    public:
+        explicit MemPool(std::size_t num_elems):
+        store_(num_elems, {T(), true}) { /* pre-allocation of vector storage*/
+            ASSERT(reinterpret_cast<const ObjectBlock *>
+                   (&(store_[0].object_)) == &(store_[0], "T object should be first member of ObjectBlock"));
+        }
+
+        MemPool() = delete;
+        MemPool(const MemPool&) = delete;
+        MemPool(const MemPool&&) = delete;
+        MemPool& operator=(const MemPool&) = delete;
+        MemPool& operator=(const MemPool&&) = delete;
+
+        template<typename... Args>
+        T *allocate(Args... args) noexcept{
+            auto obj_block = &(store_[next_free_index_]);
+            ASSERT(obj_block->is_free, "Expected free ObjectBlock at index:"
+            + std::to_string(next_free_index_));
+            T *ret = &(obj_block->object_);
+            ret = new(ret) T(args...); //placement new
+            updateNextFreeIndex();
+            return ret;
+        }
     };
-};
+}
