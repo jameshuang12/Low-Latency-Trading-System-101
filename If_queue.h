c0f50a57e63802a897ee065ba73dfec2d3fc13a9
James Huang <james153018@gmail.com>
2024-06-04 22:49:34 -0500

    Add lock-free queue header file
    Created LFqueue for the lock free queue
    utilizing public and private dataa types

--- If_queue.h
+++ If_queue.h
@@ -1,8 +1,32 @@
-//
-// Created by james on 6/3/2024.
-//
+#pragma once
+#include <iostream>
+#include <vector>
+#include <atomic>
 
-#ifndef JAMES_SAFA_PROJECT_IF_QUEUE_H
-#define JAMES_SAFA_PROJECT_IF_QUEUE_H
+namespace Common {
+    template<typename T>
+    class LFQueue final {
+    private:
+        std::vector<T> store_;
+        std::atomic<size_t> next_write_index_ = {0};
+        std::atomic<size_t> next_read_index_ = {0};
+        std::atomic<size_t> num_elements_ = {0};
+    public:
+        LFQueue(std::size_t num_elems): store_(num_elems, T()){
 
-#endif //JAMES_SAFA_PROJECT_IF_QUEUE_H
+        }
+        LFQueue() = delete;
+        LFQueue(const LFQueue&) = delete;
+        LFQueue(const LFQueue&&) = delete;
+        LFQueue& operator=(const LFQueue&) = delete;
+        LFQueue& operator=(const LFQueue&&) = delete;
+
+        auto getNextToWriteTo() noexcept {
+            return &store_[next_write_index_];
+        }
+        auto updateWriteIndex() noexcept {
+            next_write_index_ = (next_write_index_ + 1) % store_.size();
+            num_elements_++;
+        }
+    };
+}
