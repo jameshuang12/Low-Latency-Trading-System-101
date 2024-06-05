334395f33ab4733ab95ccf482d7f6924fb13821d
James Huang <james153018@gmail.com>
2024-06-05 08:13:41 -0500

    Finish header implementation for lock-free queue
    Created three functions to be utiized for the lock free queue

--- If_queue.h
+++ If_queue.h
@@ -28,5 +28,16 @@ namespace Common {
             next_write_index_ = (next_write_index_ + 1) % store_.size();
             num_elements_++;
         }
+        auto getNextToRead() const noexcept -> const T * {
+            return (next_read_index_ == next_write_index_) ? nullptr: &store_[next_read_index_];
+        }
+        auto updateReadIndex() noexcept {
+            next_read_index_ = (next_read_index_ + 1) % store_.size();
+            ASSERT(num_elements_ != 0, "Read an invalid element in:" + std::to_string(pthread_self()));
+            num_elements_--;
+        }
+        auto size() const noexcept {
+            return num_elements_.load();
+        }
     };
 }
