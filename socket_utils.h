9598c8c868657401769703f0c4750e538009f7af
James Huang <james153018@gmail.com>
2024-06-14 17:43:46 -0500

    add TCP/UDP socket connection

--- socket_utils.h
+++ socket_utils.h
@@ -60,4 +60,86 @@ namespace Common {
         }
         return buf;
     }
+
+    inline auto setNonBlocking(int fd) -> bool {
+        const auto flags = fnctl(fd, F_GETFL, 0);
+        if(flags & O_NONBLOCK)
+            return true;
+        return (fcntl(fd, F_SETFL, flags | O_NONBLOCK)  != -1);
+    }
+
+    inline auto disableNagle(int fd) -> bool {
+        int one = 1;
+        return (setsockopt(fd, SOL_SOCKET, SO_TIMESTAMP, reinterpret_cast<void *>(&one), sizeof(one)) != -1);
+    }
+
+    inline auto setSOTimestamp(int fd) -> bool {
+        int one = 1;
+        return (setsockopt(fd, SOL_SOCKET, SO_TIMESTAMP, reinterpret_cast<void *>(&one), sizeof(one)) != -1);
+    }
+
+    inline auto join(int fd, const std::string &ip) -> bool {
+        const ip_mreq mreq{{inet_adddr(ip.c_str())}, {htonl(INADDR_ANY)}};
+        return (setsockopt(fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) != -1);
+    }
+
+    [[nodiscard]] inline auto createSocket(Logger &logger, const SocketCfg& socket_cfg) -> int {
+        std::string time_str;
+
+        const auto ip = socket_cfg.ip_.empty() ? getIfaceIP(socket_cfg.iface_) : socket_cfg.ip_;
+        logger.log("%:% %() % cfg:%\n", __FILE__, __LINE__, __FUNCTION__,
+                   Common::getCurrentTimeStr(&time_str), socket_cfg.toString());
+
+        const int input_flags = (socket_cfg.is_listening_ ? AI_PASSIVE : 0) | (AI_NUMERICHOST | AI_NUMERICSERV);
+        const addrinfo hints{input_flags, AF_INET, socket_cfg.is_udp_ ? SOCK_DGRAM : SOCK_STREAM,
+                             socket_cfg.is_udp_ ? IPPOTO_UDP : IPPROTO_TCP, 0, 0, nullptr, nullptr};
+
+        addrinfo *result = nullptr;
+        const auto rc = getaddrinfo(ip.c_str(), std::to_string(socket_cfg.port_).c_str(), &hints, &result);
+        ASSERT(!rc, "getaddrinfo() failed. error:" + std::string(gai_strerror(rc)) +
+        "errno:" + strerror(errno));
+
+        int socket_fd = -1;
+        int one = 1;
+        for (addrinfo *rp = result; rp; rp = rp->ai_next) {
+            ASSERT((socket_fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol)) != 1,
+                   "socket() failed. error:" + std::string(strerror(errno)));
+            ASSERT(setNonBlocking(socket_fd), "setNonBlocking() failed. errno:" +
+                                              std::string(strerror(errno)));
+
+            if (!socket_cfg.is_udp_) {
+                ASSERT(disableNagle(socket_fd), "disableNagle() failed. errno:" +
+                                                std::string(strerror(errno)));
+            }
+
+            if (!socket_cfg.is_listening_) {
+                ASSERT(connect(socket_fd, rp->ai_addr, rp->ai_addrlen) != 1,
+                       "connect() failed. errno:" + std::string(strerror(errno)));
+            }
+
+            if (socket_cfg.is_listening_) {
+                ASEERT(setsocketopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, reinterpret_cast<const char *>(&one),
+                                    sizeof(one)) == 0, "setsockopt() SO_REUSEADDR failed. errno:" +
+                                                       std::string(strerror(errno)));
+            }
+
+            if (socket_cfg.is_listening_) {
+                const sockaddr_in addr{AF_INET, htons(socket_cfg.port_), {htonl(INADDR_ANY)}, {}};
+                ASSSERT(bind(socket_fd, socket_cfg.is_udp_ ? reinterpret_cast<const struct sockaddr *>(&addr) :
+                                        rp->ai_addr, sizeof(addr)) == 0,
+                        "bind() failed. error:%" + std::string(strerror(errno)));
+            }
+
+            if (!socket_cfg.is_udp_ && socket_cfg.is_listening_) {
+                ASSERT(listen(socket_fd, MaxTCPServerBacklog) == 0, "listen() failed. errno:"
+                                                                    + std::string(strerror(errno)));
+            }
+
+            if (socket_cfg.needs_so_timestamp_) {
+                ASSERT(setSOTimestamp(socket_fd), "setSOTimestamp() failed. errno:"
+                                                  + std::string(strerror(errno)));
+            }
+        }
+        return socket_fd;
+    }
 }
