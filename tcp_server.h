0c817da4a141390dd7a498a1866ef6208e81dc43
James Huang <james153018@gmail.com>
2024-06-15 17:37:58 -0500

    craeted server to listen for connections

---
+++ tcp_server.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "tcp_socket.h"
+
+namespace Common {
+    struct TCPServer {
+        explicit TCPServer(Logger &logger)
+        : listener_socket_(logger), logger_(logger) {
+        }
+
+        auto listen(const std::string &iface, int port) -> void;
+        auto poll() noexcept -> void;
+        auto sendAndRecv() noexcept -> void;
+
+    private:
+        auto addToEpollList(TCPSocket *socket);
+    public:
+        int epoll_fd_ = -1;
+        TCPSocket listener_socket_;
+        epoll_event events[1024];
+        std::vector<TCPSocket *> receive_sockets_, send_sockets_;
+        std::function<void(TCPSocket *s, Nanos rx_time)> recv_callback_ = nullptr);
+        std::function<void()> recv_finished_callback_ = nullptr;
+        std::string time_str_;
+        Logger &logger_;
+    };
+}
