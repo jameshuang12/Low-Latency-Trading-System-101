d206225d242f6f80b39adfe6ef764fdd87470f07
James Huang <james153018@gmail.com>
2024-06-15 19:30:19 -0500

    add connection functionality to tcp

--- tcp_server.h
+++ tcp_server.h
@@ -17,7 +17,7 @@ namespace Common {
     public:
         int epoll_fd_ = -1;
         TCPSocket listener_socket_;
-        epoll_event events[1024];
+        epoll_event events-[1024];
         std::vector<TCPSocket *> receive_sockets_, send_sockets_;
         std::function<void(TCPSocket *s, Nanos rx_time)> recv_callback_ = nullptr);
         std::function<void()> recv_finished_callback_ = nullptr;
