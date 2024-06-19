5684082b859e7e0027f049bccf1791e9469576ac
James Huang <james153018@gmail.com>
2024-06-18 21:43:59 -0500

    creating type

--- Chapter6/types.h
+++ Chapter6/types.h
@@ -1,8 +1,22 @@
-//
-// Created by james on 6/18/2024.
-//
+#pragma  once
 
-#ifndef LOW_LATENCY_TRADING_SYSTEM_101_TYPES_H
-#define LOW_LATENCY_TRADING_SYSTEM_101_TYPES_H
+#include <cstdint>
+#include <limits>
 
-#endif //LOW_LATENCY_TRADING_SYSTEM_101_TYPES_H
+#include "macros.h"
+
+namespace Common {
+    constexpr size_t ME_MAX_TICKERS = 8;
+    constexpr size_t  ME_MAX_CLIENT_UPDATES = 256 * 1024;
+    constexpr size_t  ME_MAX_MARKET_UPDATES = 256 * 1024;
+    constexpr size_t  ME_MAX_NUM_UPDATES = 256;
+    constexpr size_t  ME_MAX_ORDER_IDS = 1024 * 1024;
+    constexpr size_t  ME_MAX_PRICE_LEVELS = 256;
+
+    typedef uint64_t OrderId;
+    constexpr auto OrderId_INVALID = std::numeric_limits<OrderId>::max();
+
+    inline auto orderIdToString(OrderID order_id) -> std::string {
+
+    }
+}
