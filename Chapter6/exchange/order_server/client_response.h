0886af77c18bf22051367fefd26911a1d1c5210a
James Huang <james153018@gmail.com>
2024-06-22 15:40:22 -0500

    refactor client response

--- Chapter6/exchange/order_server/client_response.h
+++ Chapter6/exchange/order_server/client_response.h
@@ -35,12 +35,11 @@ namespace Exchange{
         ClientResponseType type_ = ClientResponseType::INVALID;
         ClientId client_id_ = ClientId_INVALID;
         TickerId ticker_id_ = TickerId_INVALID;
-        OrderId client_order_id_ = OrderId_INVALID;
-        OrderId market_order_id_ = OrderId_INVALID;
+        OrderId order_id_ = OrderId_INVALID;
         Side side_ = Side::INVALID;
         Price price_ = Price_INVALID;
-        Qty exec_qty_ = Qty_INVALID;
-        Qty leaves_qty_ = Qty_INVALID;
+        Qty qty_ = Qty_INVALID;
+
 
         auto toString() const {
             std::stringstream ss;
@@ -49,11 +48,9 @@ namespace Exchange{
                << "type:" << clientResponseTypeToString(type_)
                << " client:" << clientIdToString(client_id_)
                << " ticker:" << tickerIdToString(ticker_id_)
-               << " coid:" << orderIdToString(client_order_id_)
-               << " moid:" << orderIdToString(market_order_id_)
+               << " oid:" << orderIdToString(client_order_id_)
                << " side:" << sideToString(side_)
-               << " exec_qty:" << qtyToString(exec_qty_)
-               << " leaves_qty:" << qtyToString(leaves_qty_)
+               << " qty:" << qtyToString(qty_)
                << " price:" << priceToString(price_)
                << "]";
             return ss.str();
@@ -61,5 +58,4 @@ namespace Exchange{
     };
 #pragma pack(pop)
     typedef LFQueue<MEClientResponse> ClientResponseLFQueue;
-
 }
