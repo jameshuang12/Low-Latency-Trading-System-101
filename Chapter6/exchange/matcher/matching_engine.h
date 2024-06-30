ca590ff2a3bb16a38aa919e5ade97f66b19038ed
James Huang <james153018@gmail.com>
2024-06-30 13:04:55 -0500

    finishing up my matching_engine

--- Chapter6/exchange/matcher/matching_engine.h
+++ Chapter6/exchange/matcher/matching_engine.h
@@ -7,8 +7,6 @@
 #include "order_server/client_response.h"
 #include "market_data/market_update.h"
 #include "me_order_book.h"
-#include "market_data/market_update.h"
-
 
 namespace Exchange{
 
@@ -23,6 +21,57 @@ namespace Exchange{
         auto start() -> void;
         auto stop() -> void;
 
+        auto processClientRequest(const MEClientRequest *client_request) noexcept {
+            auto order_book = ticker_order_book_[client_request->ticker_id_];
+            switch (client_request->type_) {
+                case ClientRequestType::NEW: {
+                    order_book->add(client_request->client_id_, client_request->order_id_,
+                                    client_request->ticker_id_, client_request->side_,
+                                    client_request->price_, client_request->qty_);
+                }
+                break;
+
+                case ClientRequestType::CANCEL {
+                    order_book->cancel(client_request->client_id, client_request->order_id_,
+                                       client_request->ticker_id_);
+                }
+                break;
+
+                default: {
+                    FATAL("Received invalid client-request-type:" + clientRequestTypeToString(client_request->type_));
+                }
+                break;
+            }
+        }
+
+        auto sendClientResponse(const MEClientResponse *client_response) noexcept {
+            logger_.log("%:% %() % Sending %\n", __FILE__, __LINE__, __FUNCTION__,
+                        Common::getCurrentTimeStr(&time_str_), client_response->toString();
+            auto next_write = outgoing_ogw_responses->getNextToWriteTo();
+            *next_wirte = std::move(*client_response);
+            outgoing_md_updates_->updateWriteIndex();
+        }
+
+        auto sendMarketUpdate(const MEMarketUpdate *market_update) noexcept {
+            logger_.log("%:% %() % Sending %\n", __FILE__, __LINE__, __FUNCTION__,
+                        Common::etCurrentTimeStr(&time_str_), market_update->toString());
+            auto next_write = outgoing_md_update_->getNextToWriteTo();
+            *next_write = *market_update;
+            outgoing_md_update_->updateWriteIndex();
+        }
+
+        auto run() noexcept {
+            logger_.log("%:% %() %\n", __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(%time_str_));
+            while (run_) {
+                const auto me_client_request = incoming_request_->getNextToRead();
+                if (LIKELY(me_client_request)) {
+                    logger_.log("%:% %(0 % Processing %\n", __FILE__, __LINE__, __FUNCTION__,
+                                Common::getCurrentTimeStr(&time_str_), me_client_request->toString());
+                    processClientRequest(me_client_request);
+                    incoming_requests_->updateReadIndex();
+                }
+            }
+        }
         MatchingEngine() = delete;
         MatchingEngine(const MatchingEngine &) = delete;
         MatchingEngine(const MatchingEngine &&) = delete;
@@ -30,11 +79,13 @@ namespace Exchange{
         MatchingEngine &operator=(const MatchingEngine &&) = delete;
 
     private:
+        OrderBookHashMap ticker_order_book_;
         ClientRequestLFQueue *incoming_requests_ = nullptr;
         ClientResponseLFQueue *outgoing_ogw_responses_ = nullptr;
         MEMarketUpdateLFQueue *outgoing_md_updates_ = nullptr;
 
         volatile bool run_ = false;
-
+        std::string time_str_;
+        Logger logger_;
     };
 }
