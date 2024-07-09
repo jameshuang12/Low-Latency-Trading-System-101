9faf13512c3eabc44a10d743a95a21b6bcccf5b1
James Huang <james153018@gmail.com>
2024-07-08 20:20:15 -0500

    add to me order book

--- Chapter6/exchange/matcher/me_order_book.h
+++ Chapter6/exchange/matcher/me_order_book.h
@@ -62,5 +62,59 @@ namespace Exchange {
         auto priceToIndex(Price price) const noexcept {
             return (price % ME_MAX_PRICE_LEVELS);
         }
+
+        auto getOrdersAtPrice(Price price) const noexcept -> MEOrdersAtPrice * {
+            return price_orders_at_price_.at(priceToIndex(price));
+        }
+
+        auto addOrdersAtPrice(MEOrdersAtPrice *new_orders_at_price) noexcept {
+            price_orders_at_price_.at(priceToIndex(new_orders_at_price->price_)) = new_orders_at_price;
+
+            const auto best_orders_by_price = (new_orders_at_price)->side_ == Side::BUY ? bids_by_price_ : asks_by_price_);
+            if(UNLIKELY(!best_orders_by_price)) {
+                (new_orders_at_price->side_ == Side::BUY ? bids_by_price_ : ask_by_price_) = new_orders_at_price;
+                new_orders_at_price->prev_entry_ = new_orders_at_price->next_entry_ = new_orders_at_price;
+
+            } else {
+                auto target = best_orders_by_price;
+                bool add_after = ((new_orders_at_price->side_ == Side::SELL && new_orders_at_price->price_ > target->price_) ||
+                        (new_orders_at_price->side_ == Side::BUY && new_orders_at_price->price_ < target->price_));
+
+                if (add_after) {
+                    target = target->next_entry_;
+                    add_after = ((new_orders_at_price->side_ == Side::SELL && new_orders_at_price->price_ > target->price_) ||
+                                 (new_orders_at_price->side_ == Side::BUY && new_orders_at_price->price_ < target->price_));
+                }
+                while (add_after && target != best_orders_by_price) {
+                    add_after = ((new_orders_at_price->side_ == Side::SELL && new_orders_at_price->price_ > target->price_) ||
+                                 (new_orders_at_price->side_ == Side::BUY && new_orders_at_price->price_ < target->price_));
+                    if (add_after)
+                        target = target->next_entry_;
+                }
+
+                if (add_after) {
+                    if (target == best_orders_by_price) {
+                        target = best_orders_by_price->prev_entry_;
+                    }
+                    new_orders_at_price->prev_entry_ = target;
+                    target->next_entry_->prev_entry_ = new_orders_at_price;
+                    new_orders_at_price->next_entry_ = target->next_entry_;
+                    target->next_entry_ = new_orders_at_price;
+                } else {
+                    new_orders_at_price->prev_entry_ = target->prev_entry_;
+                    new_orders_at_price->next_entry_ = target;
+                    target->prev_entry_->next_entry_ = new_orders_at_price;
+                    target->prev_entry_ = new_orders_at_price;
+
+                if ((new_orders_at_price->side_ == Side::BUY && new_orders_at_price->price_ > best_orders_by_price->price_) ||
+                        (new_orders_at_price->side_ == Side::SELL && new_orders_at_price->price_ < best_orders_by_price->price_)) {
+                    target->next_entry_ = (target->next_entry_ == best_orders_by_price ? new_orders_at_price : target->next_entry_);
+                    (new_orders_at_price->side_ == Side::BUY ? bids_by_price_ : ask_by_price_) == new_orders_at_price;
+                }
+            }
+        }
+    }
+
+
     };
 }
