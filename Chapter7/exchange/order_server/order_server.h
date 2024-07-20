97b15dc68a9dde32befccc1db12565f39acfbefe
James Huang <james153018@gmail.com>
2024-07-19 23:50:23 -0500

    update public functions

--- Chapter7/exchange/order_server/order_server.h
+++ Chapter7/exchange/order_server/order_server.h
@@ -1,14 +1,25 @@
-//
-// Created by james on 7/19/2024.
-//
+#pragma once
 
-#ifndef LOW_LATENCY_TRADING_SYSTEM_101_ORDER_SERVER_H
-#define LOW_LATENCY_TRADING_SYSTEM_101_ORDER_SERVER_H
+#include <functional>
 
+#include "common/thread_utils.h"
+#include "common/macros.h"
+#include "common/tcp_server.h"
 
-class order_server {
+#include "order_server/client_request.h"
+#include "order_server/client_response.h"
+#include "order_server/fifo_sequencer.h"
 
-};
+namespace Exchange {
+    class OrderServer {
+    public:
+        OrderServer(ClientRequestLFQueue *client_requests, ClientResponseLFQueue *client_responses,
+                    const std::string &iface, int port);
 
+        ~OrderServer();
 
-#endif //LOW_LATENCY_TRADING_SYSTEM_101_ORDER_SERVER_H
+        auto start() -> void;
+        auto stop() -> void;
+
+    };
+}
