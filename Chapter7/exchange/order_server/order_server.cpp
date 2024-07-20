b710e0725335a703a9afca72c072e1dcec93f037
James Huang <james153018@gmail.com>
2024-07-19 23:11:41 -0500

    added order server

---
+++ Chapter7/exchange/order_server/order_server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by james on 7/19/2024.
+//
+
+#include "order_server.h"
---
+++ Chapter7/exchange/order_server/order_server.h
@@ -0,0 +1,14 @@
+//
+// Created by james on 7/19/2024.
+//
+
+#ifndef LOW_LATENCY_TRADING_SYSTEM_101_ORDER_SERVER_H
+#define LOW_LATENCY_TRADING_SYSTEM_101_ORDER_SERVER_H
+
+
+class order_server {
+
+};
+
+
+#endif //LOW_LATENCY_TRADING_SYSTEM_101_ORDER_SERVER_H
