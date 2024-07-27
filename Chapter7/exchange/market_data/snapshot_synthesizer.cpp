f1d2b27c9d4593cea6dc5037444c81aa2ad9d4f1
James Huang <james153018@gmail.com>
2024-07-27 16:18:00 -0500

    add snapshot_synthesizer header file

--- Chapter7/exchange/market_data/snapshot_synthesizer.cpp
+++ Chapter7/exchange/market_data/snapshot_synthesizer.cpp
@@ -11,3 +11,51 @@
 #include "matcher/me_order.h"
 
 using namespace Common;
+
+namespace Exchange {
+    class SnapshotSynthesizer {
+    public:
+        SnapshotSynthesizer(MDPMarketUpdateLFQueue *market_updates, const std::string &iface,
+                            const std::string &snapshot_ip, int snapshot_port);
+
+        ~SnapshotSynthesizer();
+
+        auto start() -> void;
+
+        auto stop() -> void;
+
+        auto addToSnapshot(const MDPMarketUpdate *market_update);
+
+        auto publishSnapshot();
+
+        auto run() -> void;
+
+        // Deleted default, copy & move constructors and assignment-operators.
+        SnapshotSynthesizer() = delete;
+
+        SnapshotSynthesizer(const SnapshotSynthesizer &) = delete;
+
+        SnapshotSynthesizer(const SnapshotSynthesizer &&) = delete;
+
+        SnapshotSynthesizer &operator=(const SnapshotSynthesizer &) = delete;
+
+        SnapshotSynthesizer &operator=(const SnapshotSynthesizer &&) = delete;
+
+    private:
+        MDPMarketUpdateLFQueue *snapshot_md_updates_ = nullptr;
+
+        Logger logger_;
+
+        volatile bool run_ = false;
+
+        std::string time_str_;
+
+        McastSocket snapshot_socket_;
+
+        std::array<std::array<MEMarketUpdate *, ME_MAX_ORDER_IDS>, ME_MAX_TICKERS> ticker_orders_;
+        size_t last_inc_seq_num_ = 0;
+        Nanos last_snapshot_time_ = 0;
+
+        MemPool<MEMarketUpdate> order_pool_;
+    };
+}
