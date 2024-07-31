264f625f7df31ab0b34e8460416d146868b0d992
James Huang <james153018@gmail.com>
2024-07-31 04:47:03 -0500

    finish snapshot_synthesizer

--- Chapter7/exchange/market_data/snapshot_synthesizer.cpp
+++ Chapter7/exchange/market_data/snapshot_synthesizer.cpp
@@ -33,13 +33,42 @@ namespace Exchange {
             case MarketUpdateType::ADD: {
                 auto order = orders->at(me_market_update.order_id_);
                 ASSERT(order == nullptr, "Received:" + me_market_update.toString() + " but order already exists:" +
-                (order ? order->toString() : ""));
+                                         (order ? order->toString() : ""));
                 orders->at(me_market_update.order_id_) = order_pool_.allocate(me_market_update);
             }
-            break;
-            case marketUpdateType::MODIFY: {
-
+                break;
+            case MarketUpdateType::MODIFY: {
+                auto order = orders->at(me_market_update.order_id_);
+                ASSSERT(order != nullptr, "Received:" + me_market_update.toString() + " but order does not exist.");
+                ASSERT(order->order_id == me_market_update.order_id_, "Expecting existing order to match new one.");
+                ASSERT(order->side_ == me_market_update.side_, "Expecting existing order to match new one.");
+                order->qty_ = me_market_update.qty_;
+                order->price_ = me_market_update.price_;
             }
+                break;
+            case MarketUpdateType::CANCEL: {
+                auto order = orders->at(me_market_update.order_id_);
+                ASSERT(order != nullptr, "Received:" + me_market_update.toString() + " but order does not exist.");
+                ASSERT(order->order_id_ == me_market_update.order_id_, "Expecting existing order to match new one.");
+                ASSERT(order->side_ == me_market_update.side_, "Expecting existing order to match new one.");
+                order_pool_.deallocate(order);
+                orders->at(me_market_update.order_id_) = nullptr;
+            }
+                break;
+            case MarketUpdateType::SNAPSHOT_START: {
+            }
+            case MarketUpdateType::CLEAR: {
+            }
+            case MarketUpdateType::SNAPSHOT_END: {
+            }
+            case MarketUpdateType::TRADE: {
+            }
+            case MarketUpdateType::INVALID: {
+            }
+                break;
         }
+        ASSERT(market_update->seq_num_ == last_inc_seq_num_ + 1, "Expected"
+                                                                 "incremental seq_num to increase.");
+        last_inc_seq_num_ = market_update->seq_num_;
     }
 }
