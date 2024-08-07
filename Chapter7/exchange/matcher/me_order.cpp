fef3a49aabb31038609f3c365f6b620dab49edcc
James Huang <james153018@gmail.com>
2024-08-06 22:47:02 -0500

    refactor file info

--- Chapter7/exchange/matcher/me_order.cpp
+++
@@ -1,9 +0,0 @@
-#include "me_order_book.h"
-
-#include "matcher/matching_engine.h"
-
-namespace Exchange {
-    MEOrderBook::MEOrderBook(TickerId ticker_id, Logger *logger, Exchange::MatchingEngine *matching_engine)
-    : ticker_id_(ticker_id), matching_engine_(matching_engine), orders_at_price_pool(ME_MAX_PRICE_LEVELS),
-    order_pool_(ME_MAX_ORDER_IDS), logger_(logger){}
-}
