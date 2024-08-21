60b62ce88334a1525c657e5defa265c1a3739362
James Huang <james153018@gmail.com>
2024-08-21 15:15:14 -0500

    implemented logger example

--- Chapter7/common/logging.cpp
+++ Chapter7/common/logging.cpp
@@ -1,5 +1,20 @@
-//
-// Created by james on 8/21/2024.
-//
-
 #include "logging.h"
+
+int main(int, char **) {
+    using namespace Common;
+
+    char c = 'd';
+    int i = 3;
+    unsigned long ul = 65;
+    float f = 3.4;
+    double d = 34.56;
+    const char* s = "test C-string";
+    std::string ss = "test string";
+
+    Logger logger("logging_example.log");
+    logger.log("Logging a char:% an int:% and an unsigned:%\n", c, i, ul);
+    logger.log("Logging a float:% and a double:%\n", f ,d);
+    logger.log("Logging a C-string:'%'\n", s);
+    logger.log("Logging a string:'%\n", ss);
+    return 0;
+}
