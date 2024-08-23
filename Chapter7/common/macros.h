f71fd91948e0c9dc474d55772e5bfafa2aba0e01
James Huang <james153018@gmail.com>
2024-08-22 19:43:05 -0500

    finial macros.h

--- Chapter7/common/macros.h
+++ Chapter7/common/macros.h
@@ -1,14 +1,19 @@
-//
-// Created by james on 8/22/2024.
-//
+#pragma once
 
-#ifndef LOW_LATENCY_TRADING_SYSTEM_101_MACROS_H
-#define LOW_LATENCY_TRADING_SYSTEM_101_MACROS_H
+#include <cstring>
+#include <iostream>
 
+#define LIKELY(x) __builtin_expect(!!(x), 1)
+#define UNLIKELY(x) __builtin_expect(!!(x), 0)
 
-class macros {
+inline auto ASSERT(bool cond, const std::string &msg) noexcept {
+    if(UNLIKELY(!cond)) {
+        std::cerr << "ASSERT : " << msg << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
 
-};
-
-
-#endif //LOW_LATENCY_TRADING_SYSTEM_101_MACROS_H
+inline auto FATAL(const std::string &msg) noexcept {
+    std::cerr << "FATAL" << msg << std::endl;
+    exit(EXIT_FAILURE);
+}
