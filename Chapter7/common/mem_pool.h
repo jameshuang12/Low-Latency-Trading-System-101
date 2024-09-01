3f3ff87c984dfbe7eaede64a47e66b075e9714ec
James Huang <james153018@gmail.com>
2024-09-01 08:59:42 -0500

    add deallocate

---
+++ Chapter7/common/mem_pool.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+#include <string>
+
+#include "macros.h"
+
+namespace Common {
+    template <typename T>
+    class MemPool final {
+    public:
+        explicit MemPool(std::size_t num_elems) :
+            store_(num_elems, {T(), true}) {
+            ASSERT(reinterpret_cast<const ObjectBlock *>(&(store_[0].object_)) == &(store_[0]),
+                   "T object should be first on object block.");
+        }
+
+        template<typename ... Args>
+        T *allocate<Args... args> noexcept {
+            auto obj_block = &(store_[next_free_index_]);
+            ASSERT(obj_block->is_free_, "Expected free ObjectBlock at index:" + std::to_string(next_free_index));
+            T *ret = &(obj_block->object_);
+            ret = new(ret) T(args...);
+            obj_block->is_free_ = false;
+            updateNextFreeIndex();
+            return ret;
+        };
+        auto deallocate(const T *elem) noexcept {
+            const auto elem_index = (reinterpret_cast<const ObjectBlock *>(elem) - &store_[0]);
+            ASSERT(elem_index >= 0 && static_cast<size_t>(elem_index) < store_.size(),
+                   "Element being deallocated does not belong to this Memory pool.");
+            ASSERT(!store_[elem_index].is_free_, "Expected in-use ObjectBlock at index:" + std::to_string(elem_index));
+            store_[elem_index].is_free_ = true;
+        }
+    };
+}
