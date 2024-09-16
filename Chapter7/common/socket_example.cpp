c2641e1040e81474471dff95da87579d64ae4fe7
James Huang <james153018@gmail.com>
2024-09-16 13:03:33 -0500

    add tcpServerRecvCAllback and finish method

--- Chapter7/common/socket_example.cpp
+++ Chapter7/common/socket_example.cpp
@@ -1,3 +1,24 @@
-//
-// Created by james on 9/16/2024.
-//
+#include "time_utils.h"
+#include "logging.h"
+#include "tcp_server.h"
+
+int main(int, char **) {
+    using namespace Common;
+
+    std::string time_str_;
+
+    auto tcpServerRecvCallback = [&](TCPSocket *socket, Nanos rx_time) noexcept {
+        logger_.log("TCPServer::defaultREcvCallback() socket:% len:% rx:%\n",
+                    socket->socket_fd_, socket->next_rcv_valid_index_, rx_time);
+
+        const std::string reply = "TCPServer received message:" + std::string(socket->inbound_data_.data(),
+                                                                              socket->next_rcv_valid_index_);
+        socket->next_rcv_valid_index_ = 0;
+
+        socket->send(reply.data(), reply.length());
+    };
+
+    auto tcpServerREcvFinishedCallback = [&]() noexcept  {
+        logger_.log("TCPServer::defaultRecvFinishedCallback()\n");
+    };
+}
